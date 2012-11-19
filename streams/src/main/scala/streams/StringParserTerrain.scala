cc3c7de1f5cc78bebed6c31cb2c735e7456442a8
Mathieu Chataigner <mathieu.chataigner@gmail.com>
2012-11-19 12:26:54 +0100

    fix windows string parsing line ending

--- streams/src/main/scala/streams/StringParserTerrain.scala
+++ streams/src/main/scala/streams/StringParserTerrain.scala
@@ -76,7 +76,7 @@ trait StringParserTerrain extends GameDef {
   }
 
   private lazy val vector: Vector[Vector[Char]] =
-    Vector(level.split("\n").map(str => Vector(str: _*)): _*)
+    Vector(level.split("\r\n|\n").map(str => Vector(str: _*)): _*)
 
   lazy val terrain: Terrain = terrainFunction(vector)
   lazy val startPos: Pos = findChar('S', vector)
